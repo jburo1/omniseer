aeeddfe5d14241feb1a0515280bd087896cf8d3f
Jonas <jonas.buro1@gmail.com>
2025-08-18 13:38:36 -0700

    feat(world): simple_world populated with assets

--- ros_ws/src/omniseer_gz_assets/scripts/download_models.bash
+++ ros_ws/src/omniseer_gz_assets/scripts/download_models.bash
@@ -8,7 +8,13 @@ urls=(
   "https://fuel.gazebosim.org/1.0/OpenRobotics/models/FIRST%202015%20trash%20can"
   "https://fuel.gazebosim.org/1.0/OpenRobotics/models/Checkerboard%20Plane"
   "https://fuel.gazebosim.org/1.0/OpenRobotics/models/Hammer"
+  "https://fuel.gazebosim.org/1.0/GoogleResearch/models/Threshold_Porcelain_Teapot_White"
+  "https://fuel.gazebosim.org/1.0/GoogleResearch/models/Retail_Leadership_Summit_eCT3zqHYIkX"
+  "https://fuel.gazebosim.org/1.0/GoogleResearch/models/Travel_Mate_P_series_Notebook"
+  "https://fuel.gazebosim.org/1.0/GoogleResearch/models/Timberland_Mens_Earthkeepers_Stormbuck_Plain_Toe_Oxford"
+  "https://fuel.gazebosim.org/1.0/GoogleResearch/models/Kanex_MultiSync_Wireless_Keyboard"
 )
+
 for u in "${urls[@]}"; do
   echo "Downloading: $u"
   gz fuel download -u "$u"
